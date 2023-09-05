287668087d65b89677c57dcaa039128511eb09ec
happymin <nemo8876@naver.com>
2023-09-05 18:41:03 +0900

    9월5일수업

--- basic01_java/src/oop0905/Test07_quiz.java
+++ basic01_java/src/oop0905/Test07_quiz.java
@@ -25,6 +25,7 @@ public class Test07_quiz {
 		
 		//문2)대소문자를 서로 바꿔서 출력하시오
         //-> iTwILL
+		/*
 		char c;
 		for(int i=0; i<ch.length; i++) {
 			if(ch[i]>='A' && ch[i]<='Z') {
@@ -33,7 +34,7 @@ public class Test07_quiz {
 				ch[i]=ch[i]-32;
 			};
 			System.out.print(ch[i]);
-		}
+		}*/
 
 		
 		
